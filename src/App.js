450f0ba439d38a7699700b3716b8315cdd2d7a28
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-08 22:20:07 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,4 +3,10 @@ import React from 'react'
 
 export default class App extends React.Component {
 
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+      .then(res => res.json())
+      .then(json => this.setState({data: json}))
+  }
+  
 }
